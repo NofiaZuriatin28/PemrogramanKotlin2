c185f1f1025697c2eaa151b0f2fe10aa7b766173
MC <nofiaisme28@gmail.com>
2024-09-09 19:57:58 +0700

    add WhiledanDoWhileLoop.kt

---
+++ src/main/kotlin/WhiledanDoWhileLoop.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    //while Loop
+
+    var x: Int = 0
+    println("Example of WHile Loop--")
+    while (x <= 10){
+        print(" $x ")
+        x++
+    }
+    print('\n')
+
+    //Do-While Loop
+    var y:Int = 0
+    do{
+      y = y + 10
+      println("I am inside Do block---" +y)
+    } while (y <= 50)
+}
