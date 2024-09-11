4994ea4cc774d8a872c8b8afb71ab3593866f9e0
MC <nofiaisme28@gmail.com>
2024-09-11 09:44:01 +0700

    add Null_Safety.kt

---
+++ src/main/kotlin/Operator.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+
+    // Perhatikan kode berikut
+    //var maybeNumber: Int? = 15
+   // maybeNumber = null
+   // println(maybeNumber * 2)
+
+   // var s1 = "Hello"
+   // var s2: String? = "World"
+   // println(s1.length)
+   // println(s2.length)
+
+    // agar null bisa terpanggil maka tambahkan operator,agar melewakan pemeriksaan null
+//    var maybeNumberr: Int? = 15
+//    println(maybeNumberr !! * 2)
+//
+//    maybeNumberr = null
+//    println(maybeNumberr!! * 2)
+}
